b6a87fef418ccf9e71bc0435dcdb94d28ea6601c
ctsTech2 <tempjuice@hotmail.com>
2023-11-29 13:36:59 -0600

    api key set in docker run, script generated output

--- generate_code.py
+++ generate_code.py
@@ -1,22 +1,26 @@
 import os
-import openai
+from openai import OpenAI
 
 def call_autogen(prompt):
+    # Instantiate the OpenAI client
+    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
     try:
-        response = openai.Completion.create(
+        # Make a completion request
+        response = client.completions.create(
             model="text-davinci-003",  # Specify the model
             prompt=prompt,
             temperature=0.7,
             max_tokens=150
         )
+
+        # Return the text of the first choice
         return response.choices[0].text.strip()
     except Exception as e:
+        # Handle any exceptions that occur
         return f"An error occurred: {str(e)}"
 
-# Set the API key using an environment variable for security
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
 # Example usage
-prompt = "Your prompt here"
+prompt = "Write a Flask route in Python with an HTML form for inputting an item's name and quantity. Include fields for 'item name' and 'quantity' and a submit button. Also, provide a Flask route to handle POST requests and print the submitted values."
 generated_code = call_autogen(prompt)
 print(generated_code)
