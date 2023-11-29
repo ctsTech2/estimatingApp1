f73bfe90d65c309ac257be12945e1e6c889c9115
ctsTech2 <tempjuice@hotmail.com>
2023-11-29 15:54:10 -0600

    html form added. port changed in dockerfile

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
-import sqlite3
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-def init_db():
-    conn = sqlite3.connect('estimating.db')
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS estimates (
-                      id INTEGER PRIMARY KEY,
-                      item TEXT,
-                      quantity INTEGER,
-                      price REAL)''')
-    conn.commit()
-    conn.close()
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('form.html')  # Renders the form
+
+@app.route('/submit-item', methods=['POST'])
+def submit_item():
+    item_name = request.form.get('item_name')
+    quantity = request.form.get('quantity')
+    print(f"The item name is {item_name} and the quantity is {quantity}")
+    return "Form submitted"  # Or redirect to another page
 
 if __name__ == '__main__':
-    init_db()  # Initialize the database
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000, debug=True)
