939a37fbb3f63e0a881736100e049a88092827f0
ctsTech2 <tempjuice@hotmail.com>
2023-11-29 19:52:47 -0600

    got flask logging functions working correctly

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 from flask import Flask, request, render_template
+import logging
 
 app = Flask(__name__)
 
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+
+# Flask will now print to console
+app.logger.info("Starting Flask app...")
+
 @app.route('/')
 def index():
+    app.logger.info("Rendering the form...")
     return render_template('form.html')  # Renders the form
 
 @app.route('/submit-item', methods=['POST'])
 def submit_item():
+    app.logger.info("Form submitted.")
     item_name = request.form.get('item_name')
     quantity = request.form.get('quantity')
-    print(f"The item name is {item_name} and the quantity is {quantity}")
+    app.logger.info(f"Received: item name - {item_name}, quantity - {quantity}")
     return "Form submitted"  # Or redirect to another page
 
 if __name__ == '__main__':
